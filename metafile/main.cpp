c0af0e42ab0a663fcb88efa395f19ed78579813c
Joseph Livni <j.d.livni@gmail.com>
2017-02-14 12:03:56 -0800

    metafile updates

---
+++ metafile/main.cpp
@@ -0,0 +1,17 @@
+#include <stdlib>
+#include "metafile.hpp"
+
+using namespace std;
+using namspace metafile;
+
+int main(int argc, char * argv[]) {
+	string name;
+	string file;
+	if(argv > 1) {
+		name = argv[1]
+		cour << argv[1] << endl;
+	}
+	
+	cin >> file;
+	Metafile metafile new MetaFile(file);
+}
--- metafile/metafile.hpp
+++ metafile/metafile.hpp
@@ -1,9 +1,25 @@
 #ifndef METAFILE_H
 #define METAFILE_H
 
+using namespace std;
 
-
-
+namespace metafile {
+class Metafile {
+public:
+	Metafile(string file);
+	~Metafile();
+	void getMetafile();
+	void displayMetafile();
+protected:
+	int numberOfPreferedNeighbors = 2;
+	int Unchoking interval = 5;
+	int optimisticUnchoking = 15;
+	int fileSize;
+	int pieceSize;
+	string fileName;
+	string announce;
+};
+}
 
 
 
