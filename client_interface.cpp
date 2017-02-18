7e735ef3544e6c24f22de697e6bf3d8699dbb73c
Anisha Aggarwal <anishaa@Anishas-MacBook-Air-2.local>
2017-02-17 20:57:17 -0800

    client interface

---
+++ client_interface.cpp
@@ -0,0 +1,71 @@
+//Client interface to be able to connect to server/other peers
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void getUserData(int s_port, int c_port, char u_or_d) {
+	
+	cout << "Please enter the server port number you would like to connect to: ";
+	cin >> s_port;
+	cout << "Server Port:  " << s_port << endl;
+	
+
+	cout << "Please enter in your desired client port number between (___ ): "; 
+	cin >> c_port;
+	cout << "Client Port:  " << c_port << endl;
+
+	//check to make sure user entered either upload or download
+	while ((strcmp(u_or_d, 'd') != 0) || (strcmp(u_or_d, 'u') != 0)) {
+		cout << "Would you like to upload(u) or download(d) a file? ";
+        	cin >> u_or_d;
+        	cout << "Upload or Download: " << u_or_d << endl;
+	}
+	
+}
+//get user input on path of file to upload
+void getPath() {
+	std::string path;
+
+	cout << "Please enter the path to the file you would like to upload: ";
+	std::getline(std::cin, path);
+	cout << '\n';
+
+	ifstream file(path.c_str());
+
+	while (!file) {
+		cout << "Error while opening the file" << endl;
+		
+		cout << "Reenter the path to the file: ";
+        	std::getline(std::cin, path);
+        	cout << '\n';
+
+       		ifstream file(path.c_str());
+	}	
+}
+//get user input on what file they would like to download
+void getFileName() {
+
+}
+
+int main () {
+	int serverPort;
+	int clientPort;
+	char upORdown = 'a';
+
+	getUserData(serverPort, clientPort, upORdown);
+	if (upORdown == 'u') {
+		//read in the path to the file the user would like to upload
+		getPath();
+	} else {
+		//display the options of files to be downloaded
+		//get user input on which file they would like to download
+	}
+	
+
+	return 0;	
+}
+
+
