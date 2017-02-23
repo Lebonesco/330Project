3faba4007317021d4cd2017173e07fd0e7283223
Joseph Livni <jlivni@ix-xenial.cs.uoregon.edu>
2017-02-23 12:50:41 -0800

    database automation scripts. need to fix password insertion

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-git clone https://github.com/postgres/postgres.git
-cd postgres
-./configure  --prefix="/test"
-make 
-make install
+password="password"
+
+# install postgres onto ix your ix server
+postgresctl install
+
+# set database password
+# username is automatically your ix username
+
+{
+	/bin/expect << EOF
+	expect "*?assword:*"  
+	send -- "$password\n"
+	expect "Enter it again:*" 
+	send -- "$password\n"
+	expect eof
+EOF
+}
+
+cd ../
+
+# run postgres configuration
+. postgres-data/config.sh
+
+
+
+# if any part of this doesn't work for you let me know and/or go to this link
+# on how to do it by hand: https://systems.cs.uoregon.edu/wiki/index.php?n=Help.ToolsPostgresql
 
