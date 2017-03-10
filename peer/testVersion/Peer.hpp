a61cb83f26315d2e6ac51710bfe27e2dd16213f8
Austin Snyder <aumaro.nyc@gmail.com>
2017-03-10 10:40:51 -0800

    Current Version I am working on w/ TODO markers

---
+++ peer/testVersion/Peer.hpp
@@ -0,0 +1,41 @@
+#ifndef PEER_HPP_
+#define PEER_HPP_
+#include <string>
+#include <vector>
+#include <list>
+
+class Peer{
+	int numPieces;
+	std::vector<int> bitfield;
+	std::vector<std::string> dataBitfield;
+	std::vector<std::string> ipPortList;
+public:
+	Peer(const int numChunks, std::vector<std::string>& ipPortList, std::string type);
+
+	//Server Specific functions
+	int createSocket(std::string peerType);								//*
+	int connectToClient(const char* ipAddr, const char* port);			//*
+	int acceptConnection(int seederDescriptor);
+	int bindAndListenSocket(const char* ipAddr, int socketDescriptor);	//*
+
+	//Peer, Seeder, Leecher functions
+	int startSeeding(const char* ipAddr, const char* port);
+	int startLeeching(std::list<std::string>& ipAndPortList);
+	std::list<std::string> updateIpPortList();
+	void getPeerData(std::vector<int> seederList);
+	bool fileComplete();
+	void createBitfield(int numChunks, std::string type, std::string data = "");
+
+	//Hash functions
+	std::string createHash(std::string text);
+	int checkPieceHash(std::string pieceData, int pieceNum); //check w/server based on piece number
+	bool checkHashes(); //what should parameters be..? (recievedHash, serverHash)
+
+	//Message & Piece Functions									
+	void readRecvMSG(std::string data, int socketDescriptor);								//*
+
+};
+
+
+
+#endif /* PEER_HPP_ */
