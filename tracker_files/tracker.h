f6f41590ce6fa1cb12e69d2d4f0ef9656df04ed1
Elijah Caluya <elijahcaluya@128-223-223-198-wireless.uoregon.edu>
2017-03-14 16:46:24 -0700

    updated tracker files

--- tracker_files/tracker.h
+++ tracker_files/tracker.h
@@ -1,10 +1,9 @@
 //
 //  tracker.h
 //  
+//  Header file for tracker.c
 //
-//  Created by Elijah Caluya on 3/9/17.
-//
-//
+
 
 #ifndef tracker_h
 #define tracker_h
@@ -15,7 +14,7 @@ void updatePortList(char **array, int count, const char *s);
 
 void updateNameList(char **array, int count, const char *s);
 
-void freeArray(char** array, int index);
+void freeArray(char*** array, int index);
 
 char* encode_int(char* x);
 
