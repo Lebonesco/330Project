2f0eab80c6ecb57a54bb618ff7b426a103bb51e0
Elijah Caluya <elijahcaluya@128-223-223-199-wireless.uoregon.edu>
2017-03-16 15:37:15 -0700

    updated tracker.c

--- tracker_files/tracker.c
+++ tracker_files/tracker.c
@@ -258,7 +258,7 @@ int main(void)
             // Receive message from client to see which repsone to send
             n = recv(new_fd,buffer,15,0);
             if (n < 0) perror("ERROR reading from socket");
-            
+            buffer[15] = '\0';
             
             printf("Message from client: %s\n", buffer);
             
@@ -270,7 +270,7 @@ int main(void)
                     send(new_fd, encoded, sizeof(encoded), 0);
                     puts("list sent: ");
                     print_list(port_array,port_count);
-                    buffer[15] = '\0';
+                    
                     
                 }
             
@@ -288,7 +288,7 @@ int main(void)
                     puts("Updated lists: ");
                     print_list(port_array,port_count);
                     print_list(name_array,filename_count);
-                    buffer[15] = '\0';
+                    input[19] = '\0';
                     
                 
                 }
@@ -304,13 +304,13 @@ int main(void)
                     printf("port number updated: %s\n", port_number);
                     puts("Updated list: ");
                     print_list(port_array,port_count);
-                    buffer[15] = '\0';
+                    port_number[3] = '\0';
                 }
                 else {
                     // Client sent an invalid message
                     printf("Invalid entry\n");
                     send(new_fd, "Invalid entry",13,0);
-                    buffer[15] = '\0';
+                    
                 }
             }
             
